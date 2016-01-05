f25889f5d7b5a00b5d64262e1a84f5ae82386df7
Josh Cheek <josh.cheek@gmail.com>
2016-01-05 09:47:12 -0700

    Integrate ChunkRotator

--- exercise_1/lib/encrypt.rb
+++ exercise_1/lib/encrypt.rb
@@ -3,45 +3,20 @@ require_relative "file_io"
 require_relative "character_map"
 
 class Encrypt
-
-  attr_reader   :rotations, :file_io, :key, :date, :calc, :characters
+  attr_reader :rotations, :file_io, :key, :date, :calc
 
   def initialize(key = nil, date = nil)
-    @date    = date
-    @key     = key
-    @file_io = FileIO.new
-    @calc    = OffsetCalculator.new(key, date)
-    @characters = CharacterMap.new.characters
-  end
-
-  def get_rotations
+    @date      = date
+    @key       = key
+    @file_io   = FileIO.new
+    @calc      = OffsetCalculator.new(key, date)
     @rotations = calc.rotations
   end
 
-  def encrypt_letter_a(letter)
-    a_rotated_characters = characters.rotate(rotations[0])
-    Hash[characters.zip(a_rotated_characters)][letter]
-  end
-
-  def encrypt_letter_b(letter)
-    b_rotated_characters = characters.rotate(rotations[1])
-    Hash[characters.zip(b_rotated_characters)][letter]
-  end
-
-  def encrypt_letter_c(letter)
-    c_rotated_characters = characters.rotate(rotations[2])
-    Hash[characters.zip(c_rotated_characters)][letter]
-  end
-
-  def encrypt_letter_d(letter)
-    d_rotated_characters = characters.rotate(rotations[3])
-    Hash[characters.zip(d_rotated_characters)][letter]
-  end
-
   def encrypt(message)
-    message.chars.each_slice(4).map { |chunk|
-      ChunkRotator.new(chunk, rotations).rotate
-    }.join
+    message.scan(/.{1,4}/)
+           .map { |chunk| ChunkRotator.new(chunk, rotations).rotate }
+           .join
   end
 
   def print_message
@@ -51,12 +26,10 @@ end
 
 
 if __FILE__ == $0
-
-    file_io = FileIO.new
-    message = file_io.message
-    encryptor = Encrypt.new
-    # ('12345', 280715)
-    encryptor.get_rotations
-    file_io.output(encryptor.encrypt(message))
-    puts encryptor.print_message
+  file_io = FileIO.new
+  message = file_io.message
+  encryptor = Encrypt.new
+  # ('12345', 280715)
+  file_io.output(encryptor.encrypt(message))
+  puts encryptor.print_message
 end
--- exercise_1/test/encrypt_test.rb
+++ exercise_1/test/encrypt_test.rb
@@ -3,41 +3,13 @@ require 'minitest/pride'
 require './lib/encrypt'
 
 class EncryptTest < Minitest::Test
-
-  def test_input_encrypts_a_properly_based_on_rotation
-    shift = Encrypt.new("12345", "280715")
-    shift.get_rotations
-    assert_equal "n", shift.encrypt_letter_a("a")
-  end
-
-  def test_input_encrypts_b_properly_based_on_rotation
-    shift = Encrypt.new("12345", "280715")
-    shift.get_rotations
-    assert_equal "z", shift.encrypt_letter_b("a")
-  end
-
-  def test_input_encrypts_c_properly_based_on_rotation
-    shift = Encrypt.new("12345", "280715")
-    shift.get_rotations
-    assert_equal " ", shift.encrypt_letter_c("a")
-  end
-
-  def test_input_encrypts_d_properly_based_on_rotation
-    shift = Encrypt.new("12345", "280715")
-    shift.get_rotations
-    assert_equal "l", shift.encrypt_letter_d("a")
-  end
-
   def test_word_encrypts_properly_based_on_4_part_rotation
     encryptor = Encrypt.new("12345", "280715")
-    encryptor.get_rotations
-    input = "1111"
-    assert_equal "bny,", encryptor.encrypt(input)
+    assert_equal "bny,", encryptor.encrypt("1111")
   end
 
   def test_input_string_length_equals_output_string_length
     encryptor = Encrypt.new("12345", "280715")
-    encryptor.get_rotations
     input = "1111"
     assert input.length == encryptor.encrypt(input).length
   end
@@ -46,7 +18,6 @@ class EncryptTest < Minitest::Test
     file_io = FileIO.new
     message = file_io.message("encrypt_test.txt")
     encryptor = Encrypt.new("12345", "280715")
-    encryptor.get_rotations
     assert_equal "bny,", encryptor.encrypt(message)
   end
 
@@ -54,7 +25,6 @@ class EncryptTest < Minitest::Test
     file_io = FileIO.new
     message = file_io.message("encrypt_test.txt")
     encryptor = Encrypt.new("12345", "280715")
-    encryptor.get_rotations
     file_io.output(encryptor.encrypt(message), "encrypt_test_results.txt")
     encryptor.print_message
     assert_equal "Created '' with the key 12345 and date 280715.", encryptor.print_message
